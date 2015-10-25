fc910603cbd283661a8d0eb0c3d521759fac8454
akiltipu <amtipu.bb@gmail.com>
2015-10-26 01:44:24 +0600

    Write first Python program

---
+++ week-01/helloworld.py
@@ -0,0 +1,6 @@
+#PR4E
+#Author: Md Akil Mahmod Tipu
+#Email: amtipu.bb@gmail.com
+#Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'.
+
+print "hello world"
